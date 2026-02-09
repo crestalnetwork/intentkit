8f86d165bd2c4d001b3636556de5cc77b4b8579d
Muninn <ruihua@crestal.network>
2026-02-09 10:18:33 +0800

    fix: ui bug

--- frontend/src/app/agents/new/widgets.tsx
+++ frontend/src/app/agents/new/widgets.tsx
@@ -101,13 +101,13 @@ export const SelectWidget = (props: WidgetProps) => {
                 multiple={multiple}
                 className={`flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 ${rawErrors.length > 0 ? "border-destructive" : ""
                     }`}
-                value={value}
+                value={value || ""}
                 required={required}
                 disabled={disabled || readonly}
                 autoFocus={autofocus}
                 onBlur={onBlur && ((e) => onBlur(id, e.target.value))}
                 onFocus={onFocus && ((e) => onFocus(id, e.target.value))}
-                onChange={(e) => onChange(e.target.value)}
+                onChange={(e) => onChange(e.target.value === "" ? options.emptyValue : e.target.value)}
             >
                 {!multiple && schema.default === undefined && <option value="">Select...</option>}
                 {(enumOptions as { value: string; label: string }[])?.map(({ value, label }, i) => {
